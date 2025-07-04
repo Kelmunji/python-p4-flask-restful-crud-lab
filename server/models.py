07844d970cc39361fae51a4c9d6f09ec92302a6a
kelvin Githinji <githinjikelvin74@gmail.com>
2025-07-04 10:18:25 +0300

    working cruds

--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Numeric
 from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
@@ -7,10 +8,16 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    image = db.Column(db.String)
-    price = db.Column(db.Float)
-    is_in_stock = db.Column(db.Boolean)
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+    price = db.Column(Numeric(10, 2), nullable=False)
+    is_in_stock = db.Column(db.Boolean, default=True)
 
-    def __repr__(self):
-        return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': float(self.price),
+            'is_in_stock': self.is_in_stock
+        }
